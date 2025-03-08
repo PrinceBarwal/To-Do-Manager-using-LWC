816f8130a3e749f8a2fd8f5fc615cd6e62022091
Prince Saini <princesaini0144@gmail.com>
2025-03-08 17:03:33 +0530

    Adding two input fields and get their data in the js file

--- force-app/main/default/lwc/toDoApplication/toDoApplication.js
+++ force-app/main/default/lwc/toDoApplication/toDoApplication.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
 
-export default class ToDoApplication extends LightningElement {}
+export default class ToDoApplication extends LightningElement {
+
+    taskName = "";
+    taskDate = null;
+
+    changeHandler(event){
+        let {name, value} = event.target;
+        if(name === "taskName"){
+            this.taskName = value;
+        }
+        else if(name === "taskDate"){
+            this.taskDate = value;
+        }
+    }
+}
