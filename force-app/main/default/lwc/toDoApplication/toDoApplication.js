f4b233ee6b22876988e40ccf79d60c2018b5d5ee
Prince saini <prince.saini@sltech.local>
2025-04-17 13:04:22 +0530

    Create the task Record and store them into the task manager custom object

--- force-app/main/default/lwc/toDoApplication/toDoApplication.js
+++ force-app/main/default/lwc/toDoApplication/toDoApplication.js
@@ -1,4 +1,12 @@
+import { createRecord } from 'lightning/uiRecordApi';
 import { LightningElement } from 'lwc';
+import TASK_MANAGER_OBJECT from '@salesforce/schema/Task_Manager__c';
+import TASK_MANAGER_NAME from '@salesforce/schema/Task_Manager__c.Name';
+import TASK_DATE from '@salesforce/schema/Task_Manager__c.Task_Date__c';
+import TASK_COMPLETED_DATE from '@salesforce/schema/Task_Manager__c.Completed_Date__c';
+import TASK_IS_COMPLETED from '@salesforce/schema/Task_Manager__c.isCompleted__c';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 
 export default class ToDoApplication extends LightningElement {
 
@@ -27,16 +35,38 @@ export default class ToDoApplication extends LightningElement {
             this.taskDate = new Date().toISOString().slice(0,10);
         }
 
-        if(this.validateTask()){
-            this.incompleteTask = [...this.incompleteTask, {
-                taskName : this.taskName,
-                taskDate : this.taskDate
-            }];
-            this.resetHandler();
-            let sortedArray = this.sortArray(this.incompleteTask);
-            this.incompleteTask = [...sortedArray];
-            //console.log("this.incompleteTask", this.incompleteTask);
-        }
+        // if(this.validateTask()){
+        //     this.incompleteTask = [...this.incompleteTask, {
+        //         taskName : this.taskName,
+        //         taskDate : this.taskDate
+        //     }];
+        //     this.resetHandler();
+        //     let sortedArray = this.sortArray(this.incompleteTask);
+        //     this.incompleteTask = [...sortedArray];
+        //     //console.log("this.incompleteTask", this.incompleteTask);
+        // }
+
+        let inputFields = {};
+        inputFields[TASK_MANAGER_NAME.fieldApiName] = this.taskName;
+        inputFields[TASK_IS_COMPLETED.fieldApiName] = false;
+        inputFields[TASK_DATE.fieldApiName] = this.taskDate;
+
+        
+        let inputRecord = {
+            apiName : TASK_MANAGER_OBJECT.objectApiName,
+            fields  : inputFields
+        };
+        createRecord(inputRecord)
+        .then(result => {
+            //alert('inside then method');
+            console.log('Task Created Successfully', result);
+            this.showToast('Success', 'Task Manager Record Created Successfully', 'success');
+        })
+        .catch(error => {
+            //alert('inside catch method');
+            console.log('Unable to create a task', error);
+            this.showToast('Error', 'Unable to Create Task Manager Record', 'error');
+        })
     }
 
     validateTask(){
@@ -104,5 +134,14 @@ export default class ToDoApplication extends LightningElement {
         this.incompleteTask = [...sortedArray];
         //console.log("this.incompleteTask", this.incompleteTask);
         this.completedTask = [...this.completedTask, removeItem[0]];
+    }
+
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant : variant
+        });
+        this.dispatchEvent(event);
     }
 }
